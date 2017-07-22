de6b6b3a4519fe4b794fa2d96ffe04266fd5077d
justincyz <yuzhuangchen1@gmail.com>
2017-07-22 15:40:28 -0700

    it can now jump to another page

--- web.py
+++ web.py
@@ -5,15 +5,15 @@ import os
 from bs4 import BeautifulSoup
 import subprocess
 
-num_count = 0
 
-
-
-
-
+#url = "http://dblp.uni-trier.de/db/journals/" + '?pos=' + str(200)
+#info = urllib2.urlopen(url).read()
+#print info
 
 def first_level(num_count):
-	url = "http://dblp.uni-trier.de/db/journals/" + '?pos' + str(num_count)
+	
+	
+	url = "http://dblp.uni-trier.de/db/journals/" + '?pos=' + str(num_count)
 	info = urllib2.urlopen(url).read()
 
 	soup = BeautifulSoup(info,'lxml')
@@ -21,17 +21,21 @@ def first_level(num_count):
 		if 'href' in  link.attrs:	
 			x = str(link.attrs['href']) 
 			if x.startswith('http://dblp.uni-trier.de/db/journals/'):
-			#print x
+				print x
 				num_count = num_count + 1
-				print num_count
-			#return num_count	
+				
+	return num_count	
 		#else:
 			#url2 = "http://dblp.uni-trier.de/db/journals/" + '?pos' + str(num_count)
 			#info2 = urllib2.urlopen(url2).read()
 			#print num_count
 			#print info2
+num_count = 0			
+while num_count < 301:
+	print num_count
+	first_level(num_count)
+	num_count = 100 + num_count
 
-first_level(num_count)
 
 #print num_count			
 
