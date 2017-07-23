a951b8a95726013dc648be90d1123203d03d6c69
justincyz <yuzhuangchen1@gmail.com>
2017-07-22 22:21:37 -0700

    third level finished

--- web.py
+++ web.py
@@ -5,41 +5,59 @@ import os
 from bs4 import BeautifulSoup
 import subprocess
 
+def third_level(url):
+	info = urllib2.urlopen(url).read()
 
-#url = "http://dblp.uni-trier.de/db/journals/" + '?pos=' + str(200)
-#info = urllib2.urlopen(url).read()
-#print info
+	soup = BeautifulSoup(info,'lxml')
+	for link in soup.find_all(name="span"):
+		
+		if 'class' in  link.attrs:
+			if 'itemprop' in link.attrs:
+				print link.contents
+				f = file('result.txt','w')			
+				f.write(str(link.contents))			
+	return	None
+	
+
+def second_level(url):
+	info = urllib2.urlopen(url).read()
+
+	soup = BeautifulSoup(info,'lxml')
+	for link in soup.find_all(name="a"):
+		if 'href' in  link.attrs:	
+			x = str(link.attrs['href']) 			
+			if x.startswith('http://dblp.uni-trier.de/db/journals/'):
+				#print x
+				third_level(x)
+				
+	return	None		
 
 def first_level(num_count):
-	
-	
 	url = "http://dblp.uni-trier.de/db/journals/" + '?pos=' + str(num_count)
 	info = urllib2.urlopen(url).read()
+
 
 	soup = BeautifulSoup(info,'lxml')
 	for link in soup.find_all(name="a"):
 		if 'href' in  link.attrs:	
 			x = str(link.attrs['href']) 
 			if x.startswith('http://dblp.uni-trier.de/db/journals/'):
-				print x
-				num_count = num_count + 1
-				
+				#print x				
+				second_level(x)
+
+
 	return num_count	
-		#else:
-			#url2 = "http://dblp.uni-trier.de/db/journals/" + '?pos' + str(num_count)
-			#info2 = urllib2.urlopen(url2).read()
-			#print num_count
-			#print info2
+		
+
+
+first_level(1)
+
+
+
 num_count = 0			
 while num_count < 301:
-	print num_count
-	first_level(num_count)
+	#print num_count
+	#first_level(num_count)
 	num_count = 100 + num_count
 
-
-#print num_count			
-
-#while num_count == 0:
-#				print soup.a
-#				num_count = num_count - 1
 
