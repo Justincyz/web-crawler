f55ebd9c8607c68985621011575e765f95dbeda3
justincyz <yuzhuangchen1@gmail.com>
2017-07-23 12:54:35 -0700

    final version for searching on DBLP web

--- web.py
+++ web.py
@@ -1,28 +1,36 @@
+#Yuzhuang Chen
+#yuzhuangchen1@gmail.com
+
 import sys
 import urllib2
 import re
 import os
 from bs4 import BeautifulSoup
 import subprocess
+import robotparser
+import urlparse
+
+
+def get_robots(url):
+    rp = robotparser.RobotFileParser()
+    rp.set_url(urlparse.urljoin(url, '/robots.txt'))
+    rp.read()
+    return rp	
+
 
 def third_level(url, key):
 	info = urllib2.urlopen(url).read()
-	print key
-	if key in 'Multiobjective Evolutionary Algorithm':
-		print 'good'
 
 	soup = BeautifulSoup(info,'lxml')
-	for link in soup.find_all(name="span"):
-		
+	for link in soup.find_all(name="span"):		
 		if 'class' in  link.attrs:
 			if 'itemprop' in link.attrs:
 				temp_name = str(link.contents)
-				print temp_name
 				if key in temp_name:
-					#print link.contents
-					f = file('result.txt','w')			
-					f.write(str(link.contents))		
-					break	
+					print 'Here are the results matched your key'
+					print temp_name								
+					f.write(str(temp_name))		
+		
 	return	None
 	
 
@@ -34,34 +42,44 @@ def second_level(url, key):
 		if 'href' in  link.attrs:	
 			x = str(link.attrs['href']) 			
 			if x.startswith('http://dblp.uni-trier.de/db/journals/'):
-				#print x
 				third_level(x, key)
-				break
+				
 	return	None		
 
-def first_level(num_count, key):
+def first_level(num_count, key, user_agent):
 	url = "http://dblp.uni-trier.de/db/journals/" + '?pos=' + str(num_count)
 	info = urllib2.urlopen(url).read()
+	rp = get_robots(url)
 
-
+	if user_agent:
+		headers['User-agent'] = 'BadCrawler'
 	soup = BeautifulSoup(info,'lxml')
 	for link in soup.find_all(name="a"):
-		if 'href' in  link.attrs:	
-			x = str(link.attrs['href']) 
-			if x.startswith('http://dblp.uni-trier.de/db/journals/'):
-				#print x				
-				second_level(x, key)
-				break
-
+		if rp.can_fetch(user_agent, url):
+			if 'href' in  link.attrs:	
+				x = str(link.attrs['href']) 
+				if x.startswith('http://dblp.uni-trier.de/db/journals/'):				
+					second_level(x, key)
+	
+		else:
+			print 'This website has been blocked: ', url 	
 	return num_count	
-		
 
-key = raw_input('What you want to search for? ')
+	
+user_agent =''
+key = raw_input('Searching in DBLP? ')
+f = file('result.txt','w')
+num_count = 1			
+web = num_count    #there are more pid to be found
 
-num_count = 0			
-while num_count < 301:
-	#print num_count
-	first_level(num_count, key)
+while web != 0 :
+	web = first_level(num_count, key, user_agent)
 	num_count = 100 + num_count
+
+#while num_count < 4300 :
+#	first_level(num_count, key, user_agent)
+#	num_count = 100 + num_count
+
+f.close()	
 
 
